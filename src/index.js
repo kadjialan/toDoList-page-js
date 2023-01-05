fcdedf2fb7abd60ce1c5efe260c32993234c259a
kadjialan <alankadji7@gmail.com>
2023-01-05 12:06:26 +0100

    final design and functionality

--- src/index.js
+++ src/index.js
@@ -3,23 +3,36 @@ import './styles/styles.css';
 const addition = document.querySelector('.add');
 const area = document.querySelector('#holder');
 const items = document.querySelector('.todo');
+const reset = document.querySelector('.delete');
 
 function addItem() {
   const task = items.value;
-  const todoitem = document.createElement('div');
-  todoitem.classList.add('display');
-  todoitem.innerHTML += `<div class= "text"> <input type="radio" class="radio"> ${task}  <i class="fa-solid  icons fa-trash-can"></i> </div> `;
-  area.appendChild(todoitem);
+  if (task === '') {
+    alert('please input a todo');
+  } else {
+    const todoitem = document.createElement('div');
+    todoitem.classList.add('display');
+    todoitem.innerHTML += `<div class= "text"> <input type="checkbox" class="radio"> ${task}  <i class="fa-solid  icons fa-trash-can"></i> </div> `;
+    area.appendChild(todoitem);
+
+    todoitem.querySelector('.radio').addEventListener('click', () => {
+      todoitem.style.textDecoration = 'line-through';
+    });
+
+    todoitem.querySelector('.icons').addEventListener('click', () => {
+      todoitem.remove();
+    });
+  }
 }
 
-todoitem.querySelector('.radio').addEventListener('click', () => {
-  todoitem.style.textDecoration = 'line-through';
-});
+function redo() {
+  window.location.reload();
+}
 
 addition.addEventListener('click', () => {
   addItem();
 });
 
-/* minusBtn.addEventListener('click', () => {
-  displayValue(sub());
-}); */
+reset.addEventListener('click', () => {
+  redo();
+});
